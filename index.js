50207b01fd498ef7c44453309755cc2ea00309a1
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2020-12-24 18:51:17 +0530

    netlify form

--- index.js
+++ index.js
@@ -18,7 +18,6 @@ function rating(cls,rating){
     }
 }
 
-<<<<<<< HEAD
 
 //Recent Projects
 var projects = [
@@ -73,42 +72,6 @@ projects.forEach(project => {
 
 
 
-=======
-// appending projects
-var project = '<div class="col-md-4 projects">'+
-  '<div class="img__description_layer">'+
-   ' <p class="img_description" id="">hello</p>'+
-  '</div>';
-
-jQuery.githubUser = function(username, callback) {
-    jQuery.getJSON('https://api.github.com/users/'+'huzon'+'/repos?callback=?',callback)
- }
-
- var target = this;
- $.githubUser('huzon', function(data) {
-     var repos = data.data; // JSON Parsing    
-     console.log(repos);
-    
-     repos.forEach(tdata => {
-         if(tdata.name != "huzon"){
-         $('#projectInfo').append('<div class="col-md-4 col-sm-6 projects" id='+tdata.name+'>'+
-         '<div class="img__description_layer">'+
-          ' <div class="img_description" id="">'+
-          '<h3>'+tdata.name+'</h3>'+
-          '<p>'+tdata.description+'</p>'+
-          '<a name="" id="" class="btn btn-outline btn-large" href="'+tdata.html_url+'" role="button"><i class="fab fa-github iconProject" aria-hidden="true"></i></a>'+
-          '</div>'+
-         '</div>');
-
-         $('#'+tdata.name).css({'background-image':'url(./images/projects/'+tdata.name+'.png)', 'background-size':'cover', 'background-position': 'center', 'background-repeat': 'no-repeat'});
-         
-         console.log(tdata.name);}
-     });
-   });
-//    background-position: center;
-//    background-repeat: no-repeat;
-//    background-size: cover;
->>>>>>> parent of 7b83e1a... color scheme changes
 
 // ******************************** to get on top of page **************************************************
 window.onscroll = function () {scrollFunction()} ;
