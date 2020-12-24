f9d3c5276f0dde1f178c3afae2cb65c6b9959f45
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2020-12-24 18:45:19 +0530

    form

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ rating("java", 6);
 rating("htmlcss", 7);
 rating("js",8);
 rating("nodejs", 6);
-rating("php",8);
 rating("jquery", 8);
 rating("python", 4);
 rating("mysql", 9);
@@ -17,11 +16,9 @@ function rating(cls,rating){
     for(var i = 1; i<=rating; i++){
         $("#"+cls).append('<i class="fa fa-star" aria-hidden="true"></i>');
     }
-    for(var i = rating; i<10; i++){
-        $("#"+cls).append('<i class="fa fa-star dim" aria-hidden="true"></i>');
-    }
 }
 
+<<<<<<< HEAD
 
 //Recent Projects
 var projects = [
@@ -76,6 +73,42 @@ projects.forEach(project => {
 
 
 
+=======
+// appending projects
+var project = '<div class="col-md-4 projects">'+
+  '<div class="img__description_layer">'+
+   ' <p class="img_description" id="">hello</p>'+
+  '</div>';
+
+jQuery.githubUser = function(username, callback) {
+    jQuery.getJSON('https://api.github.com/users/'+'huzon'+'/repos?callback=?',callback)
+ }
+
+ var target = this;
+ $.githubUser('huzon', function(data) {
+     var repos = data.data; // JSON Parsing    
+     console.log(repos);
+    
+     repos.forEach(tdata => {
+         if(tdata.name != "huzon"){
+         $('#projectInfo').append('<div class="col-md-4 col-sm-6 projects" id='+tdata.name+'>'+
+         '<div class="img__description_layer">'+
+          ' <div class="img_description" id="">'+
+          '<h3>'+tdata.name+'</h3>'+
+          '<p>'+tdata.description+'</p>'+
+          '<a name="" id="" class="btn btn-outline btn-large" href="'+tdata.html_url+'" role="button"><i class="fab fa-github iconProject" aria-hidden="true"></i></a>'+
+          '</div>'+
+         '</div>');
+
+         $('#'+tdata.name).css({'background-image':'url(./images/projects/'+tdata.name+'.png)', 'background-size':'cover', 'background-position': 'center', 'background-repeat': 'no-repeat'});
+         
+         console.log(tdata.name);}
+     });
+   });
+//    background-position: center;
+//    background-repeat: no-repeat;
+//    background-size: cover;
+>>>>>>> parent of 7b83e1a... color scheme changes
 
 // ******************************** to get on top of page **************************************************
 window.onscroll = function () {scrollFunction()} ;
